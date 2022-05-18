30bf0006e081d481194a12191613c23ceaa8f47c
Your NameAnas-Nabih <anas7nabih@gmail.com>
2022-05-18 05:11:01 +0200

    recreate project

--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,17 @@
-import 'package:e_commerse_app_uising_getx/view/auth/login_view.dart';
-import 'package:e_commerse_app_uising_getx/view/auth/second_screen.dart';
-import 'package:e_commerse_app_uising_getx/view/controller_view.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
 
-import 'core/viewmodel/cart_viewmodel.dart';
-import 'helper/binding.dart';
-
-
-void main() async{
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  Get.put(CartViewModel());
+void main(){
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  const MyApp({Key key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      initialBinding: Binding(),
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        fontFamily: 'SourceSans'
-      ),
-      home: ControlView(),
+      home: Scaffold(),
     );
   }
 }
