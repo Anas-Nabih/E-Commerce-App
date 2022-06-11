7ad2b5d2b758e3cfa29372b2ddddcefc6eda245d
Your NameAnas-Nabih <anas7nabih@gmail.com>
2022-06-11 18:27:59 +0200

    handle loading in home

--- lib/core/viewmodel/home_view_model.dart
+++ lib/core/viewmodel/home_view_model.dart
@@ -1,25 +1,38 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:e_commerse_app_uising_getx/model/category_model.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class HomeViewModel extends GetxController {
+
+  ValueNotifier<bool> _categoriesLoaded = ValueNotifier(false);
+  ValueNotifier<bool> get categoriesLoaded =>  _categoriesLoaded;
 
   RxList<CategoryModel> categoriesList = <CategoryModel>[].obs;
 
   final _categoryCollectionRef =
       FirebaseFirestore.instance.collection("categories");
 
-  getCategories() {
-    _categoryCollectionRef.get().then((categories) {
+  getCategories() async {
+
+   await _categoryCollectionRef.get().then((categories) {
       for(int i = 0 ; i<categories.docs.length ; i++){
         categoriesList.add(CategoryModel.fromJson(categories.docs[i].data()));
+        categoriesLoaded.value = true;
+        print(categoriesLoaded.value );
       }
+      update();
     });
+
+  }
+
+  HomeViewModel(){
+    getCategories();
   }
 
   @override
   void onInit() {
     super.onInit();
-    getCategories();
+
   }
 }
--- lib/view/main_screens/home/home_view.dart
+++ lib/view/main_screens/home/home_view.dart
@@ -1,6 +1,7 @@
- import 'package:e_commerse_app_uising_getx/core/viewmodel/home_view_model.dart';
+import 'package:e_commerse_app_uising_getx/core/viewmodel/home_view_model.dart';
 import 'package:e_commerse_app_uising_getx/model/best_selling_model.dart';
- import 'package:e_commerse_app_uising_getx/widgets/custom_categories_list.dart';
+import 'package:e_commerse_app_uising_getx/res/styles/colors.dart';
+import 'package:e_commerse_app_uising_getx/widgets/custom_categories_list.dart';
 import 'package:e_commerse_app_uising_getx/widgets/custom_grid_view.dart';
 import 'package:e_commerse_app_uising_getx/widgets/head_line.dart';
 import 'package:e_commerse_app_uising_getx/widgets/home_search_field.dart';
@@ -8,50 +9,66 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:sizer/sizer.dart';
 
-class HomeView extends GetView<HomeViewModel>{
+class HomeView extends GetView<HomeViewModel> {
   @override
   Widget build(BuildContext context) {
     return GetBuilder<HomeViewModel>(
       init: HomeViewModel(),
       builder: (controller) => Scaffold(
-        body:CustomScrollView(
-          physics: BouncingScrollPhysics(),
-          slivers: [
-            SliverAppBar(
-              elevation: 4,
-              toolbarHeight: 12.h,
-              title: Column(
-                children: [
-                  HomeSearchField(),
-                  SizedBox(height: 2.h,)
-                ],
-              ),
-              floating: true,
-              pinned: true,
-              expandedHeight: 40.h,
-              flexibleSpace: FlexibleSpaceBar(
-                background: Column(
-                  children: [
-                    SizedBox(height: 18.h,),
-                    HeadLine(title: "Categories".tr,),
-                    CategoriesList(categoriesList: controller.categoriesList,),
+          body: controller.categoriesLoaded.value
+              ? CustomScrollView(
+                  physics: BouncingScrollPhysics(),
+                  slivers: [
+                    SliverAppBar(
+                      elevation: 4,
+                      toolbarHeight: 12.h,
+                      title: Column(
+                        children: [
+                          HomeSearchField(),
+                          SizedBox(
+                            height: 2.h,
+                          )
+                        ],
+                      ),
+                      floating: true,
+                      pinned: true,
+                      expandedHeight: 40.h,
+                      flexibleSpace: FlexibleSpaceBar(
+                        background: Column(
+                          children: [
+                            SizedBox(
+                              height: 18.h,
+                            ),
+                            HeadLine(
+                              title: "Categories".tr,
+                            ),
+                            CategoriesList(
+                              categoriesList: controller.categoriesList,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    SliverList(
+                        delegate: SliverChildBuilderDelegate(
+                            (BuildContext context, index) {
+                      if (index == 0) {
+                        return HeadLine(
+                          title: "Best Selling".tr,
+                          hasSuffix: true,
+                        );
+                      } else {
+                        return CustomGridView(
+                          gridList: BestSelling.bestSellingList,
+                          isFromHome: true,
+                        );
+                      }
+                    }, childCount: 2)),
                   ],
-                ),
-              ),
-            ),
-            SliverList(delegate: SliverChildBuilderDelegate(
-                    (BuildContext context, index){
-                  if(index == 0 ){
-                    return HeadLine(title: "Best Selling".tr,hasSuffix: true,);
-                  }else{
-                    return CustomGridView(gridList: BestSelling.bestSellingList,isFromHome:true,);
-                  }
-                },childCount: 2
-            )),
-          ],
-        )
-    ),);
+                )
+              : Center(
+                  child: CircularProgressIndicator(color: MColors.primaryColor),
+                )),
+    );
   }
-
 }
-
